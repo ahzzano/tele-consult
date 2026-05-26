3f37630aa8a53da2d726f3fcb83ced16dfd430fb
W Santos <51366379+ahzzano@users.noreply.github.com>
2026-05-26 17:45:41 +0800

    Add doctor checking

--- backend/src/doctor/doctor.service.ts
+++ backend/src/doctor/doctor.service.ts
@@ -1,15 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
 import { CreateDoctorDto } from './dto/create-doctor.dto';
 import { UpdateDoctorDto } from './dto/update-doctor.dto';
 import { doctor } from 'src/db/schema';
 import { DbService } from 'src/db/db.service';
+import { eq } from 'drizzle-orm';
 
 @Injectable()
 export class DoctorService {
     constructor(private db: DbService) { }
 
     async create(dto: CreateDoctorDto) {
-        console.log(dto)
+        const existing = await this.db.connection
+            .select()
+            .from(doctor)
+            .where(eq(doctor.acctId, dto.acctId))
+            .limit(1);
+
+        if (existing.length > 0) {
+            throw new ConflictException('Email already in use');
+        }
+
         const [newDoctor] = await this.db.connection
             .insert(doctor)
             .values({
