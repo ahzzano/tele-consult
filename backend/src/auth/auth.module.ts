03823ad1d38a5576e5c84d128a4243b6ad6a5edc
W Santos <51366379+ahzzano@users.noreply.github.com>
2026-05-26 22:31:51 +0800

    Add authentication

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { AccountService } from 'src/account/account.service';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { AccountModule } from 'src/account/account.module';
+import { jwtConstants } from './constants';
 
 @Module({
+    imports: [
+        AccountModule,
+        JwtModule.register({
+            global: true, 
+            secret: jwtConstants.secret,
+            signOptions: {expiresIn: '100s'}
+        })
+    ],
     controllers: [AuthController],
-    providers: [AuthService, AccountService],
+    providers: [AuthService],
 })
 export class AuthModule { }
